97f871ae6df852ca209927ff65c77357e2a6a6f2
Daiveed <jajadavidjid@gmail.com>
2023-01-08 22:48:47 +0100

    My first commit containing all the necessary lines of code for this article

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const promise = new Promise(function (resolve, reject) {
+  let randNum = Math.random();
+  console.log(randNum);
+  if (randNum >= 0.5) {
+    resolve("Promise Resolved");
+  } else {
+    reject("Promise Rejected");
+  }
+});
+// console.log(promise);
+
+promise
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error))
+  .finally((secResult) => console.log("It worked"));
